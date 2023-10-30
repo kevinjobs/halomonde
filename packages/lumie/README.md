015cb592b6714fe4ea5a3cbd745b90b39c7dec47
kevinjobs <me@kevinjobs.com>
2023-10-30 21:42:50 +0800

    feat: 正确记录列表页面数据并在挂载时恢复

--- packages/lumie/README.md
+++ packages/lumie/README.md
@@ -1,2 +1,8 @@
-# kevinjobs.github.io
- gh pages
+本项目用于生成 gh-pages
+
+```
+# 首先生成
+pnpm run build
+# 推送到 gh-pages 分支
+pnpm run deploy
+```
--- packages/lumie/src/pages/admin/posts/index.tsx
+++ packages/lumie/src/pages/admin/posts/index.tsx
@@ -33,10 +33,20 @@ export default function PostAdmin(): React.ReactElement {
   const PAGE_LIMIT = 6;
   const navigate = useNavigate();
   const [posts, setPosts] = React.useState<IPost[]>(null);
-  const [offset, setOffset] = React.useState(0);
-  const [typ, setTyp] = React.useState('all');
+  const [offset, setOffset] = React.useState(Number(localStorage.getItem('offset')) || 0);
+  const [typ, setTyp] = React.useState(localStorage.getItem('typ') || 'all');
+  // 这两个状态是根据 posts 的情况计算出的，因此不需要保存
   const [hasPrev, setHasPrev] = React.useState(false);
   const [hasNext, setHasNext] = React.useState(true);
+
+  React.useEffect(() => {
+    // 当列表变动时将其保存在 localStorage中
+    // 这样可以在下次加载时重新取出以恢复状态
+    // 在本页面中，只需要保存这两个状态就可以恢复整个页面
+    // 针对不同的页面，可能需要保存不同的值
+    localStorage.setItem('offset', String(offset));
+    localStorage.setItem('typ', typ);
+  }, [offset, typ]);
 
   const clickAdd = (t: string) => {
     navigate(`/admin/edit/add/${t}/0`);
@@ -68,33 +78,43 @@ export default function PostAdmin(): React.ReactElement {
         const data = await deletePost(uid);
         if (typeof data !== 'string') {
           window.alert('删除成功');
-          getAndSet();
+          getAndSet(offset, PAGE_LIMIT, typ);
         } else window.alert(data);
       })();
     }
   };
 
-  const getAndSet = async (offset = 0, limit=PAGE_LIMIT) => {
-    const data = await fetchPosts(offset, limit, {status: 'all', type: typ});
-    if (typeof data !== 'string') {
-      if (offset <= 0) setHasPrev(false);
-      else setHasPrev(true);
+  const getAndSet = (offset = 0, limit: number, typ: string) => {
+    (async() => {
+      const data = await fetchPosts(offset, limit, {status: 'all', type: typ});
+      if (typeof data !== 'string') {
+        if (offset <= 0) setHasPrev(false);
+        else setHasPrev(true);
 
-      if (offset + limit >= data.data.totals) setHasNext(false);
-      else setHasNext(true);
+        if (offset + limit >= data.data.totals) setHasNext(false);
+        else setHasNext(true);
 
-      setPosts(data.data.posts);
-    }
+        setPosts(data.data.posts);
+      }
+    })();
   };
 
+  const handleFilter = (t: string) => {
+    setTyp(t);
+    setOffset(0);
+    setPosts(null);
+    setTimeout(() => {
+      getAndSet(0, PAGE_LIMIT, t);
+    }, 100);
+  }
+
   React.useEffect(() => {
-    getAndSet(offset, PAGE_LIMIT);
+    getAndSet(offset, PAGE_LIMIT, typ);
   }, [offset]);
 
   React.useEffect(() => {
-    setOffset(0);
-    getAndSet(0, PAGE_LIMIT);
-  }, [typ]);
+    getAndSet(offset, PAGE_LIMIT, typ);
+  }, []);
 
   return (
     <Article>
@@ -106,10 +126,10 @@ export default function PostAdmin(): React.ReactElement {
         </Header.Add>
         <div style={{marginLeft: 32}}>
           <span>点击筛选</span>
-          <Button type='primary' onClick={() => setTyp('all')}>所有</Button>
-          <Button type='primary' onClick={() => setTyp('article')}>文章</Button>
-          <Button type='primary' onClick={() => setTyp('photo')}>照片</Button>
-          <Button type='primary' onClick={() => setTyp('cover')}>封面</Button>
+          <Button type={typ === 'all' ? 'primary' : 'light'} onClick={() => handleFilter('all')}>所有</Button>
+          <Button type={typ === 'article' ? 'primary' : 'light'} onClick={() => handleFilter('article')}>文章</Button>
+          <Button type={typ === 'photo' ? 'primary' : 'light'} onClick={() => handleFilter('photo')}>照片</Button>
+          <Button type={typ === 'cover' ? 'primary' : 'light'} onClick={() => handleFilter('cover')}>封面</Button>
         </div>
       </Header>
       <TableContainer>
