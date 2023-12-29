839c3c4aa253540d4ac40512cfac0ba29dad20ab
Kevin Jobs <me@kevinjobs.com>
2023-12-29 11:42:25 +0800

    fix: 重复创建mask

--- packages/lumie/src/components/mask/mask.tsx
+++ packages/lumie/src/components/mask/mask.tsx
@@ -9,12 +9,16 @@ const M = styled.div`
 `;
 
 export function Mask() {
+  const MASK_ID = 'mask-container';
   useEffect(() => {
-    const container = document.createElement('div');
-    container.id = 'mask-container';
-    document.body.appendChild(container);
+    let container = document.getElementById(MASK_ID);
+    if (!container) {
+      container = document.createElement('div');
+      container.id = MASK_ID;
+      document.body.appendChild(container);
+    }
   }, []);
-  return document.getElementById('mask-container')
-    ? ReactDOM.createPortal(<M/>, document.getElementById('mask-container'))
+  return document.getElementById(MASK_ID)
+    ? ReactDOM.createPortal(<M/>, document.getElementById(MASK_ID))
     : <div></div>;
 }
