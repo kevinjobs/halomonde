b683cbb981383d6789631c92206341af51d723bb
Kevin Jobs <me@kevinjobs.com>
2024-02-14 22:32:13 +0800

    fix some bugs

--- packages/lumie/src/pages/domo/index.tsx
+++ packages/lumie/src/pages/domo/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Route, Routes, } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import { store, } from '@/store';
-import { Icon, } from '@horen/core';
-import { useStore, } from '@horen/store';
+import { store } from '@/store';
+import { Icon } from '@horen/core';
+import { useStore } from '@horen/store';
 
 import {
-    BanPage, Layout, LeftMenu, Login, SubMenuProps, Titlebar, TitlebarProps,
+  BanPage,
+  Layout,
+  LeftMenu,
+  Login,
+  SubMenuProps,
+  Titlebar,
+  TitlebarProps,
 } from './_components';
 import Home from './home';
 import style from './index.module.less';
@@ -21,7 +27,7 @@ interface PageProps extends Omit<SubMenuProps, 'children'> {
   items?: PageProps[];
   loginRequired?: boolean;
   allowLevels?: string[];
-};
+}
 
 const LEFT_ITEMS: PageProps[] = [
   {
@@ -30,37 +36,37 @@ const LEFT_ITEMS: PageProps[] = [
     element: <Home />,
     to: 'home',
     arrow: false,
-    icon: <Icon name="home" />
+    icon: <Icon name="home" />,
   },
   {
     title: '内容管理',
     description: '主要内容管理',
     element: <></>,
     expand: true,
-    icon: <Icon name='menu' />,
+    icon: <Icon name="menu" />,
     items: [
       {
         title: '所有内容浏览',
         description: '管理所有内容',
         element: <Everything />,
         to: 'content/posts',
-        icon: <Icon name='files' />,
+        icon: <Icon name="files" />,
       },
-    ]
+    ],
   },
   {
     title: '系统管理',
     description: '系统管理',
     element: <></>,
     expand: true,
-    icon: <Icon name='menu' />,
+    icon: <Icon name="menu" />,
     items: [
       {
         title: '用户管理',
         description: '查看、编辑、更新用户',
         element: <User />,
         to: 'system/user',
-        icon: <Icon name='verse' size={27} />,
+        icon: <Icon name="verse" size={27} />,
         loginRequired: true,
         allowLevels: ['admin', 'superuser'],
       },
@@ -69,17 +75,17 @@ const LEFT_ITEMS: PageProps[] = [
         description: '查看邀请码',
         element: <Invitaition />,
         to: 'system/invitaition',
-        icon: <Icon name='verse' size={27} />,
+        icon: <Icon name="verse" size={27} />,
         loginRequired: true,
         allowLevels: ['superuser'],
-      }
-    ]
-  }
-]
+      },
+    ],
+  },
+];
 
 const TITLE_BAR_PROPS: TitlebarProps = {
-  logo: <Icon name='dimension' />,
-}
+  logo: <Icon name="dimension" />,
+};
 
 export default function Domo() {
   return (
@@ -96,13 +102,11 @@ export default function Domo() {
           <LeftMenu items={LEFT_ITEMS} />
         </div>
         <div className={style.right}>
-          <Routes>
-            {renderPages(LEFT_ITEMS)}
-          </Routes>
+          <Routes>{renderPages(LEFT_ITEMS)}</Routes>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 const renderPages = (pages: PageProps[]) => {
@@ -111,7 +115,7 @@ const renderPages = (pages: PageProps[]) => {
 
   const arr = [];
   for (const page of pages) {
-    if (page.to) arr.push(page)
+    if (page.to) arr.push(page);
     else if (page.items) {
       for (const item of page.items) {
         if (item.to) {
@@ -127,14 +131,14 @@ const renderPages = (pages: PageProps[]) => {
                       currentRole={state.user.role}
                       allowRole={item.allowLevels.join(',')}
                     />
-                  )
-                })
+                  ),
+                });
               }
             } else {
               arr.push({
                 ...item,
-                element: <BanPage message='请登录后访问' />,
-              })
+                element: <BanPage message="请登录后访问" />,
+              });
             }
           } else {
             arr.push(item);
@@ -143,20 +147,17 @@ const renderPages = (pages: PageProps[]) => {
       }
     }
   }
-  return arr.map(page => {
+  return arr.map((page) => {
     return (
       <Route
         path={page.to}
-        element={(
-          <Layout
-            title={page.title}
-            description={page.description}
-          >
+        element={
+          <Layout title={page.title} description={page.description}>
             {page.element}
           </Layout>
-        )}
+        }
         key={page.to}
       />
-    )
-  })
-}
+    );
+  });
+};
