24fe9d593c224371aa4fe09e578a6960173d610b
kevinjobs <me@kevinjobs.com>
2024-06-30 19:25:05 +0800

    [lumie] :bug: fix some bugs

--- packages/lumie/src/pages/domo/home/Home.tsx
+++ packages/lumie/src/pages/domo/home/Home.tsx
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { useUnmount } from '@horen/hooks';
-import { DatePicker } from '@/components/DatePicker';
 
 import style from './Home.module.less';
 
 export default function Home() {
-  const [date, setDate] = useState(new Date());
-
   useUnmount(() => {
     console.log('unmount');
   });
 
-  return (
-    <div className={style.home}>
-      <DatePicker value={date} onChange={setDate} />
-    </div>
-  );
+  return <div className={style.home}></div>;
 }
